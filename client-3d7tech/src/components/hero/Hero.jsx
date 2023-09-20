3476bbec4a5f6417d4e3f2891e8abefc04419fd1
Edititus <tediomo.et@gmail.com>
2023-09-20 23:03:31 +0100

    create navbar

--- client-3d7tech/src/components/hero/Hero.jsx
+++ client-3d7tech/src/components/hero/Hero.jsx
@@ -1,13 +1,115 @@
-import React from 'react'
-import './hero.css'
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 const Hero = () => {
-  return (
-    <div className="hero-container">
-      <video src="/videos/cover.mp4" autoPlay loop muted />
-      <h1>Hello</h1>
-    </div>
-  )
-}
+  const [text, setText] = useState('processes.');
 
-export default Hero
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Toggle between 'Processes.' and 'Products.'
+      setText((prevText) => (prevText === 'Products.' ? 'Processes.' : 'Products.'));
+    },2000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <HeroContainer>
+      <Video src='/videos/bgimage.mov' autoPlay loop muted />
+      <ContentContainer>
+        <div className='container'>
+          <div>
+            <TitleContainer>
+              <StyledParagraph>3D7 TECH Helps Individuals & Companies</StyledParagraph>
+            </TitleContainer>
+            <input id='efdw' type='hidden' value={text} />
+            <MainText>
+              Design, Develop & Deliver   <StyledText>{text}</StyledText>
+            </MainText>
+          </div>
+        </div>
+      </ContentContainer>
+    </HeroContainer>
+  );
+};
+
+
+export const HeroContainer = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  margin-top: -5rem;
+  z-index: -1;
+  
+`;
+
+export const Video = styled.video`
+  position: absolute;
+  top: 50%; 
+  left: 50%; 
+  transform: translate(-50%, -50%);
+  min-width: 100%; 
+  min-height: 100%; 
+`;
+
+export const ContentContainer = styled.div`
+  position: relative;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 20px; /* Add padding to the content */
+  
+`;
+export const TitleContainer = styled.div`
+  border-radius: 6.25rem;
+  background: #ebf8fe;
+  display: flex;
+  padding: 0.5rem 0.875rem;
+  justify-content: center;
+  align-items: center;
+  width: 18rem;
+  height: 2.25rem;
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+export const StyledParagraph = styled.p`
+  color: #0f0f10;
+  font-family: DM Sans;
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 140%; /* 1.225rem */
+  letter-spacing: -0.00438rem;
+  align-items: center;
+  position: absolute;
+  top: 25%;
+
+
+`;
+export const MainText = styled.h1`
+  color: #0f0f10;
+  text-align: center;
+  font-size: 4.5rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 140%; /* 6.3rem */
+  letter-spacing: -0.0225rem;
+  gap: 0.125rem;
+  margin: 0 22rem;
+`;
+
+const StyledText = styled.span`
+  color: ${({ children }) =>
+    children === 'Processes.' ? '#2507DF' : '#E15C12'};
+  font-weight: bold;
+`;
+export default Hero;
--- client-3d7tech/src/components/navBar/NavBar.jsx
+++ client-3d7tech/src/components/navBar/NavBar.jsx
@@ -1,27 +1,86 @@
-import React from 'react';
-import './navbar.css'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr';
+
 const NavBar = () => {
-    return (
-      <header className='header-icons header-border'>
-        <div className='container'>
-          <a href='index.html' className='animated fadeInUp'>
-            <img
-              src='/images/logo.jpg'
-               className='logo'
-              alt='3d7tech Logo'
-            />
-          </a>
-          <nav className='navigation'>
-            <ul className='menu'>
-              <li>
-                <a href='index.html#products'>Products</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    );
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
   };
-  
-  export default NavBar;
-  
+
+  return (
+    <Container>
+      <ImageWrapper src='/images/logo-small.png' alt='3d7tech Logo' />
+      <HamburgerIconWrapper onClick={toggleDropdown}>
+        {isDropdownOpen ? <GrCloseIcon /> : <GiHamburgerMenuIcon />}
+      </HamburgerIconWrapper>
+      {isDropdownOpen && (
+        <DropdownContent isOpen={isDropdownOpen}>
+          <Paragraph>Home</Paragraph>
+          <Paragraph>Training</Paragraph>
+          <Paragraph>Products</Paragraph>
+          <Paragraph>Contact Us</Paragraph>
+        </DropdownContent>
+      )}
+    </Container>
+  );
+};
+
+export const Container = styled.div`
+  display: flex;
+  height: 5rem;
+  padding: 1rem 5rem 1.0625rem 5rem;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ImageWrapper = styled.img`
+  height:3rem;
+`;
+
+export const HamburgerIconWrapper = styled.div`
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 5rem;
+  height: 3rem;
+`;
+
+export const GiHamburgerMenuIcon = styled(GiHamburgerMenu)`
+  width: 100%;
+  height: 100%;
+`;
+export const GrCloseIcon = styled(GrClose)`
+  width: 100%;
+  height: 100%;
+`;
+
+export const DropdownContent = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  z-index: 1;
+  margin-top: 5rem;
+`;
+export const Paragraph = styled.p`
+  text-align: center;
+  font-family: DM Sans;
+  font-size: 4.5rem;
+  font-style: normal;
+  font-weight: 400;
+  padding: 1.5rem;
+  line-height: 140%; /* 6.3rem */
+
+  letter-spacing: -0.0225rem;
+  &: hover {
+    color: var(--Primary, #079be6);
+  }
+`;
+
+export default NavBar;
